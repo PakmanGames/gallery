64e76d006e8aa3143aac8745db818ae36e373646
PakmanGames <im.andy.pak@gmail.com>
2025-11-21 11:20:08 -0500

    Create scaffold of ui

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 
 ## TODO
 
-- [ ] Make it deploy (vercel)
+- [x] Make it deploy (vercel)
 - [ ] Scaffold basic ui with mock data
 - [ ] Actually set up a database (neon postgres)
 - [ ] Attach database to UI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://dr84hsp0i3.ufs.sh/f/afn1kApZqHDhLVczlpah2Z7YozFR1KmG0n8t9IjrfbNuD6Us",
+  "https://dr84hsp0i3.ufs.sh/f/afn1kApZqHDhiOl3ixf81YxMDjW4lOGRZkmv0CLB3SEyuqez",
+  "https://dr84hsp0i3.ufs.sh/f/afn1kApZqHDhsf7Hi0XbzFhvT6ul3DcPSJQnrtEaV82M9mKo",
+  "https://dr84hsp0i3.ufs.sh/f/afn1kApZqHDhiOO8HRnf81YxMDjW4lOGRZkmv0CLB3SEyuqe",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       <div>Hello (gallery in progress)</div>
     </main>
   );
